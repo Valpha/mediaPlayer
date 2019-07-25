b7e7d71c83f864ba4168a39a04494b59aa2998e0
JS021 <JS021@aliyun.com>
2019-07-25 16:46:10 +0800

    更改程序架构，整理代码，以及代码分离 再次修复UI，模拟机中的画面与实机画面一致了

--- app/src/main/java/com/example/mediaplayer/Utils.java
+++ app/src/main/java/com/example/mediaplayer/Utils.java
@@ -10,25 +10,36 @@ import static com.example.mediaplayer.ImageUtils.*;
 public class Utils {
 
     public Utils() {
-    return;
+        return;
     }
 
-    public static void loadCover(Bitmap cover, ImageView iv_cover, ImageView iv_ref, ImageView iv_smallCover){
+    /**
+     * @param cover         封面图,Bitmap 格式
+     * @param iv_cover      要更新的控件
+     * @param iv_ref        倒影对应的控件
+     * @param iv_smallCover 唱片中心的小图
+     */
+    public static void loadCover(Bitmap cover, ImageView iv_cover, ImageView iv_ref, ImageView iv_smallCover) {
 
-        Bitmap _cover = zoomBitmap(cover,190,190);
+        Bitmap _cover = zoomBitmap(cover, 190, 190);
+
         iv_cover.setImageBitmap(_cover);
 
         Bitmap _reflection = ReflectionImageWithOutOrigin(cover);
         iv_ref.setImageBitmap(_reflection);
 
-        Bitmap _smallCover = RoundedCornerBitmap(cover, 20);
+        Bitmap _smallCover = zoomBitmap(cover, 70, 70);
+        _smallCover = RoundedCornerBitmap(_smallCover, 35);
         iv_smallCover.setImageBitmap(_smallCover);
-
     }
-    public static int getProgressPercentage(long currentDuration,
-                                            long totalDuration) {
-        Double percentage = (double) 0;
 
+    /**
+     * @param currentDuration 当前时长
+     * @param totalDuration 总时长
+     * @return 当前百分比 int 类型
+     */
+    public static int getProgressPercentage(long currentDuration, long totalDuration) {
+        Double percentage = (double) 0;
         long currentSeconds = (int) (currentDuration / 1000);
         long totalSeconds = (int) (totalDuration / 1000);
 
